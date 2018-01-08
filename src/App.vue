d483ffa41826a673c654ea07722f323f1297104f
wyq-mac <wangyaqi.chn@gmail.com>
2018-01-08 22:10:16 +0800

    时间组件

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+  <DatePicker type="date" placeholder="Select date" style="width: 200px" v-model="aaa"></DatePicker>
+  <date-panel v-model="aaa"></date-panel>
+  </div>
+</template>
+
+<script>
+import datePanel from './components/datePanel'
+export default {
+  components: {
+    datePanel
+  },
+  data() {
+    return {
+      aaa: new Date()
+    }
+  }
+   
+}
+
+</script>
+
