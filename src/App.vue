1d931507ed91fa9bbd757b795d5a44c4bedf3667
wyq-mac <wangyaqi.chn@gmail.com>
2018-01-16 22:13:36 +0800

    iview modal

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,11 @@
   <div id="app">
   <!-- <DatePicker type="date" placeholder="Select date" style="width: 200px" v-model="aaa"></DatePicker>
   <date-panel v-model="aaa"></date-panel> -->
-  <v-model v-model="abc"></v-model>
-  {{abc}}
+  <!-- <v-model v-model="abc"></v-model>
+  {{abc}} -->
+  <Modal v-model="open" @on-ok="ok" :loading="loading">
+    <Button @click="clickMe">点击</Button>
+  </Modal>
   </div>
 </template>
 
@@ -18,7 +21,20 @@ export default {
   data() {
     return {
       aaa: new Date(),
-      abc: '2343'
+      abc: '2343',
+      open: true,
+      loading: true
+    }
+  },
+  methods: {
+    ok() {
+      this.loading = true
+    },
+    clickMe() {
+      this.loading = false
+      this.$nextTick(() => {
+        this.loading = true
+      })
     }
   }
 }
