9009ef23363a57cf5b12c7da344a9f08058f1713
wyq-mac <wangyaqi.chn@gmail.com>
2018-02-25 07:21:15 +0800

    vue-model

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div>
       123
-      <router-link to="vmodel"></router-link>
+      <router-link to="vmodel">v-model</router-link>
     </div>
     <router-view></router-view>
   <!-- <DatePicker type="date" placeholder="Select date" style="width: 200px" v-model="aaa"></DatePicker>
