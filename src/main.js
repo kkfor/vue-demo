c433313326cf75058a68481cb25bbea0ce5cfa69
wyq-mac <wangyaqi.chn@gmail.com>
2018-02-25 06:44:41 +0800

    配置路由

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import iView from 'iview'
 import 'iview/dist/styles/iview.css'
+import routes from './router'
 
 Vue.use(iView)
 
 new Vue({
+  routes,
   el: '#app',
   render: h => h(App)
 })
