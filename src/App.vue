665b5b4623bed74ce3d689713f72b10f0a05b686
wyq-pc <wangyaqi.chn@gmail.com>
2018-03-19 13:32:16 +0800

    vue init

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,12 @@
 <template>
   <div id="app">
-    <div>
-      <router-link to="vmodel">v-model</router-link>
-      <router-link to="buttonDisabled">buttonDisabled</router-link>
-      <router-link to="phone">phone</router-link>
-      <router-link to="phone1">phone1</router-link>
-      <router-link to="/vueRouter">vueRouter</router-link>
-    </div>
-    <router-view></router-view>
-  <!-- <DatePicker type="date" placeholder="Select date" style="width: 200px" v-model="aaa"></DatePicker>
-  <date-panel v-model="aaa"></date-panel> -->
-  <!-- <v-model v-model="abc"></v-model>
-  {{abc}} -->
-  <!-- <Modal v-model="open" @on-ok="ok" :loading="loading">
-    <Button @click="clickMe">点击</Button>
-  </Modal> -->
+    <img src="./assets/logo.png">
+    <router-view/>
   </div>
 </template>
 
 <script>
-
 export default {
-  data() {
-    return {
-      aaa: new Date(),
-      abc: '2343',
-      open: true,
-      loading: true
-    }
-  },
-  methods: {
-    ok() {
-      this.loading = true
-    },
-    clickMe() {
-      this.loading = false
-      this.$nextTick(() => {
-        this.loading = true
-      })
-    }
-  }
+  name: 'App'
 }
-
 </script>
-
-<style>
-#app {
-  padding: 20px;
-}
-a {
-  margin: 0 5px;
-}
-</style>
-
